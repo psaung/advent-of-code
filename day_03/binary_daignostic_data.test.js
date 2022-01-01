951d9a4fb31d4769c6b52059f572194a4fbc84a5
Pyae Sone <psaung299@gmail.com>
2022-01-01 19:36:59 +0700

    Add test cases for Day 03

---
+++ day_03/binary_daignostic_data.test.js
@@ -0,0 +1,23 @@
+const { tests, eq } = require("../jstinytest");
+const { compare, flipbit, extractBinary, extract } = require("./utils");
+
+tests({
+  "Flip bit": () => {
+    eq(flipbit("01001"), "10110");
+  },
+
+  Compare: () => {
+    const min = ["01101", "00100"];
+    const max = ["11110", "10110", "10111"];
+    eq(compare(min, max, "min"), min);
+  },
+
+  ExtractBinary: () => {
+    eq(extractBinary("00010"), "2");
+  },
+
+  Extract: () => {
+    const numbers = ["11110", "10110", "10111"];
+    eq(extract(numbers, 1, "0"), "10110,10111,11110");
+  },
+});
--- day_03/binary_diagnostic_solution.js
+++ day_03/binary_diagnostic_solution.js
@@ -1,46 +1,16 @@
 const fs = require("fs");
+const path = require("path");
+const { compare, flipbit, extractBinary, extract } = require("./utils");
 
-const data = fs.readFileSync("./binary_diagnostic_data.txt", {
-  encoding: "utf8",
-  flag: "r",
-});
+const data = fs.readFileSync(
+  path.join(__dirname, "./binary_diagnostic_data.txt"),
+  {
+    encoding: "utf8",
+    flag: "r",
+  }
+);
 
 const inputs = data.split("\n");
-
-function flipbit(binary) {
-  return binary
-    .split("")
-    .map((b) => (1 - b).toString())
-    .join("");
-}
-
-function extractBinary(binary) {
-  return parseInt(binary, 2);
-}
-
-function extract(numbers, index, binary) {
-  return numbers.reduce(
-    ([left, right], x) =>
-      x[index] === binary ? [[...left, x], right] : [left, [...right, x]],
-    [[], []]
-  );
-}
-
-function compare(minArr, maxArr, compareCase = "min") {
-  if (minArr.length === maxArr.length) {
-    return compareCase === "min" ? minArr : maxArr;
-  }
-
-  if (compareCase === "min") {
-    return minArr.length < maxArr.length ? minArr : maxArr;
-  }
-
-  if (compareCase === "max") {
-    return minArr.length < maxArr.length ? maxArr : minArr;
-  }
-
-  throw new Error("Unexpected error occurred");
-}
 
 function solution1(data) {
   if (data.length < 1) return 0;
