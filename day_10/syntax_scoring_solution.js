f43a080468c347655ca9ca880ab01e05016bb6cd
Pyae Sone <psaung299@gmail.com>
2022-01-06 00:12:29 +0700

    Day 10 Solution 2

--- day_10/syntax_scoring_solution.js
+++ day_10/syntax_scoring_solution.js
@@ -9,16 +9,16 @@ const inputs = fs.readFileSync(
   }
 );
 
+const match = {
+  "[": "]",
+  "<": ">",
+  "(": ")",
+  "{": "}",
+};
+const closing = Object.keys(match).map((x) => match[x]);
+
 function solution1(data) {
   const inputs = data.split("\n");
-
-  const match = {
-    "[": "]",
-    "<": ">",
-    "(": ")",
-    "{": "}",
-  };
-  const closing = Object.keys(match).map((x) => match[x]);
 
   const value = {
     ")": 3,
@@ -47,7 +47,6 @@ function solution1(data) {
         lane.push(str[start]);
       }
     }
-
     return null;
   }
 
@@ -57,4 +56,48 @@ function solution1(data) {
   }, 0);
 }
 
+function solution2(data) {
+  const inputs = data.split("\n");
+  const points = {
+    "(": 1,
+    "[": 2,
+    "{": 3,
+    "<": 4,
+  };
+
+  function findAllClosings(str) {
+    const len = str.length;
+
+    const lane = [];
+
+    if (closing.includes(str[0])) {
+      return null;
+    }
+
+    for (let start = 0; start < len; start++) {
+      if (closing.includes(str[start])) {
+        const dd = lane.pop();
+
+        if (match[dd] !== str[start]) {
+          return null;
+        }
+      } else {
+        lane.push(str[start]);
+      }
+    }
+
+    return lane.reverse().reduce((p, v) => p * 5 + points[v], 0);
+  }
+
+  let allPoints = [];
+
+  inputs.forEach((str) => {
+    allPoints.push(findAllClosings(str));
+  });
+
+  allPoints = allPoints.filter(Boolean).sort((a, b) => a - b);
+  return allPoints[Math.floor(allPoints.length / 2)];
+}
+
 console.log(solution1(inputs));
+console.log(solution2(inputs));
