e6d17eebf9822a9f0aee18d4a5717ddaf2b113b0
Pyae Sone <psaung299@gmail.com>
2022-01-08 16:16:09 +0700

    Day 11 Solution 2

--- day_11/dumbo_octopus_solution.js
+++ day_11/dumbo_octopus_solution.js
@@ -10,7 +10,8 @@ const inputs = fs.readFileSync(
   }
 );
 
-let flashes = 0;
+let flashes = 0,
+  count = 0;
 
 function solution1(inputs, rows, cols, times) {
   let dict = {};
@@ -71,8 +72,75 @@ function solution1(inputs, rows, cols, times) {
   return flashes;
 }
 
+function solution2(inputs, rows, cols, expectedResults) {
+  let dict = {};
+  let a9 = {};
+
+  a9 = {};
+  dict = {};
+
+  function checkTheNeighbour(index, inputs) {
+    getDirection(index, rows, cols)
+      .filter((v) => v !== null && !a9[v])
+      .forEach((v) => {
+        if (!a9[v]) {
+          dict[v] = dict[v] || parseInt(inputs[v]);
+
+          dict[v] = parseInt(dict[v]) + 1;
+
+          if (dict[v] > 9) {
+            a9[v] = true;
+            dict[v] = "0";
+            flashes += 1;
+            checkTheNeighbour(v, inputs);
+          }
+        }
+      });
+  }
+
+  for (let i = 0; i < inputs.length; i++) {
+    dict[i] = dict[i] || parseInt(inputs[i]);
+
+    if (dict[i] >= 9 && !a9[i]) {
+      a9[i] = true;
+      dict[i] = "0";
+      flashes += 1;
+      checkTheNeighbour(i, inputs);
+    } else {
+      dict[i] =
+        inputs[i] !== "0" && parseInt(dict[i]) === 0
+          ? 0
+          : parseInt(dict[i]) + 1;
+      if (dict[i] > 9) {
+        a9[i] = true;
+        dict[i] = "0";
+        flashes += 1;
+        checkTheNeighbour(i, inputs);
+      }
+    }
+  }
+
+  const resultt = Object.keys(dict)
+    .map((v) => dict[v])
+    .reduce((p, v) => p + "" + v, "");
+
+  count++;
+
+  if (resultt !== expectedResults) {
+    return solution2(resultt, rows, cols, expectedResults);
+  }
+
+  return count;
+}
+
 const i = inputs.split("\n").join("");
 const cols = inputs.split("\n")[0].length;
 const rows = inputs.split("\n").length;
 
 console.log(solution1(i, cols, rows, 99));
+
+flashes = 0;
+const expectedResults = Array.from(new Array(i.length))
+  .map(() => "0")
+  .join("");
+console.log(solution2(i, cols, rows, expectedResults));
